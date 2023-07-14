d21b49775ee6835c698f2f05d7dea68a0d2757e8
Kakeru0307 <Kakeru0307@users.noreply.github.com>
2023-07-14 05:11:25 +0000

    deploy: 438d1d992646615aa3b4e6d948b2fead52d38ece

---
+++ _next/static/QgSBhegHpu1o0D8m-1zg3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6f07ce014bcf23f9.css","static/chunks/pages/index-3ab4ee2ab739e973.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
